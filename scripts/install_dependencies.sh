1ec27c9328fb49b2eca20c91e6541f946505ff48
thao.vu <thaovu@miu.edu>
2025-04-15 07:43:18 -0500

    cleanup

--- scripts/install_dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/myapp
-npm install  # Install dependencies for the backend app
--- scripts/pm_restart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/myapp
-pm2 restart backend/app.js  # Replace with your actual entry file
